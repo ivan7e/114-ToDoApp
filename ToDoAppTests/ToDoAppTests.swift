967ad62b67c3f269131943eb3cc0223eaf8379a7
ivan <ivan.n.english@gmail.com>
2026-03-03 20:34:09 -0600

    Initial Commit

---
+++ ToDoAppTests/ToDoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppTests.swift
+//  ToDoAppTests
+//
+//  Created by Ghost Buster on 3/3/26.
+//
+
+import Testing
+@testable import ToDoApp
+
+struct ToDoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
